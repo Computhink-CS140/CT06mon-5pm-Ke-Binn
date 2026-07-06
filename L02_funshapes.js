202814e608e8661563e044f2a54ebcce108df105
Computhink-CS140 <cs140@computhink.com.sg>
2026-07-06 10:03:14 +0000

    Jul 6, 2026, 10:03 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -8,7 +8,7 @@ function setup() {
 function draw() {
   // Challenge 1: An eye using ellipse() function
 ellipse(300,200,300,200);
-fill(255,0,0);
+  fill(255,0,0);
 ellipse(300,200,100,200);
 ellipse(300,200,100,50);
 }
