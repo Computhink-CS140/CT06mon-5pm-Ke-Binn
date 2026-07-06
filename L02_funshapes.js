332b72e24383df26129ffe66b68791f14b9d0126
Computhink-CS140 <cs140@computhink.com.sg>
2026-07-06 10:28:55 +0000

    Jul 6, 2026, 10:28 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -25,7 +25,7 @@ function draw() {
 // circle(400,200,50)
 // rect(250,300,100,150)
 // ellipse(320,350,20,50)
-triangle(100,210,210,160,150,30)
+triangle(100,160,210,160,150,30)
 
 
   //hallenge 2: A face using circle() function
