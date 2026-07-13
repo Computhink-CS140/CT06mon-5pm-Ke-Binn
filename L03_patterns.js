3cc3bae80ad469046d5edb6b6d853f43683ab84c
Computhink-CS140 <cs140@computhink.com.sg>
2026-07-13 10:45:07 +0000

    Jul 13, 2026, 10:45 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -14,7 +14,8 @@ function draw() {
   let numCircles = 5;
   let totalWidth = numCircles * circleDiameter;
   let startX = (width - totalWidth) / 2 + circleDiameter / 2;
-  let startY = (width - totalWidth) / 2 + circleDiameter / 2;
+  let startY = (height - totalWidth) / 2 + circleDiameter / 2;
+  
 
   // Task 1: Colour Gradient
 
