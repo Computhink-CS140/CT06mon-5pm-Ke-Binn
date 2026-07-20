ba1f3ce03be53f55fd344bb7358ebfb32d579507
Computhink-CS140 <cs140@computhink.com.sg>
2026-07-20 09:46:24 +0000

    Jul 20, 2026, 9:46 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -15,7 +15,7 @@ function draw() {
   let totalWidth = numCircles * circleDiameter;
   let startX = (width - totalWidth) / 2 + circleDiameter / 2;
   let startY = (height - totalWidth) / 2 + circleDiameter / 2;
-  for (let row = 0; row < numCircles; row++) {
+  for (let i = 0; row < numCircles; row++) {
     let rowY = startY + (circleDiameter * row);
     for (let i = 0; i < numCircles; i++) {
     let x = startX + i *circleDiameter;
