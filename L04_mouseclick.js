871e45771b80cdcf4ccaa50c2090e50d2747f195
Computhink-CS140 <cs140@computhink.com.sg>
2026-07-20 10:18:30 +0000

    Jul 20, 2026, 10:18 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -9,7 +9,7 @@ function setup() {
 
 
 function draw() {
-    for (let i = 0; i < 5; i++){
+    for (let i = 0; i < 5; i++) {
         fill(0,0 + i*25,0)
         circle(30 + i*30,30+i*30,30);
     }
