cc0f21923a231371b8c7f5881108935b23c6bbc4
Computhink-CS140 <cs140@computhink.com.sg>
2026-07-27 09:55:18 +0000

    Jul 27, 2026, 9:55 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,6 +1,5 @@
 // write your codes here// write your codes here
-let shapeColor='blue';
-let x1,y1,x2,y2,x3,y3;
+let rectsize=50;
 function setup() {
     createCanvas(600, 400);
     background(220);
