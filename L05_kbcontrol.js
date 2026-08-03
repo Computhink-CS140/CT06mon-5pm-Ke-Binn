eb6cbe8c98471b75f8c0baccb486dc6da4373498
Computhink-CS140 <cs140@computhink.com.sg>
2026-08-03 10:03:51 +0000

    Aug 3, 2026, 10:03 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -15,7 +15,7 @@ function draw() {
   circle(width/2,height/2,100)
   }
 function keyPressed(){
-  if(keycode)
+  if(keyCode === 38)
       console.log("key:",key);
     console.log("keyCode:",keyCode);
 }
