7955034d19d124fd025ae12091e3a0eaf4d15fc6
Computhink-CS140 <cs140@computhink.com.sg>
2026-08-17 11:03:42 +0000

    Aug 17, 2026, 11:03 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -35,7 +35,7 @@ if(key === '='){
     staticSize >300? staticSize=300:staticSize<0?staticSize =0:StaticSize
 }
     if(staticImageX < 25 || staticImageX>375 || staticImageY<25 ||staticImageY>375)
-        colorr
+        colorr = color(255)
 
 
 }
