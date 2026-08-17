32eead18ccd6aa1f1130359b980821eb390baa8f
Computhink-CS140 <cs140@computhink.com.sg>
2026-08-17 10:36:06 +0000

    Aug 17, 2026, 10:36 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -1,6 +1,7 @@
 let soundEffect ,bgMusic, staticimage,wineffect,loseEffect;
 let sttaticImageX = 0
 let staticImageY = 0
+let
 function preload(){
     soundEffect = loadSound('assets/pop.mp3');
     bgMusic = loadSound('assets/bossaNova.mp3');
