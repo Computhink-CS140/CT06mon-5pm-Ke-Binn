4f17badda7114f3f10eff192a3be7d0e375b055e
Computhink-CS140 <cs140@computhink.com.sg>
2026-08-24 09:50:42 +0000

    Aug 24, 2026, 9:50 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -10,5 +10,17 @@ function setup(){
     createCanvas(600,400)
 }
 function draw(){
-    
+    background(220);
+    if(keyIsDown(LEFT_ARROW)){
+        x -=3
+}
+    if(keyIsDown(RIGHT_ARROW)){
+        x +=3
+}
+    if(keyIsDown(UP_ARROW)){
+        y-=3
+}
+    if(keyIsDown(DOWN_ARROW)){
+        y+=3
+}
 }
