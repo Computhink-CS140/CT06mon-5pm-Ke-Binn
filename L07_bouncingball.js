4ecd09062e722c0f1c4d75907b0d38cf3225a4d4
Computhink-CS140 <cs140@computhink.com.sg>
2026-08-24 10:39:28 +0000

    Aug 24, 2026, 10:39 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -1,35 +1,35 @@
-let ballx = 300;
-let bally = 200;
-let size = 30;
-let ballspeedx = 3;
-let ballspeedy = 3;
-function setup(){
-    createCanvas(600,400);
-}
-function draw(){
-    background(220);
-    circle(ballx,bally,size);
-    ballx += ballspeedx;
-    bally += ballspeedy;
-    if(ballx + (ballsize/2) >= width || ballx - (ballsize/2)<=0){
-        ballspeedx = -ballspeedx
-        if(colorchange){
-            r = random(255);g = random(255);b = random(255)
-        }
-    }
-    if(bally + (ballsize/2) > height || bally < ballsize/2){
-        ballspeedy = -ballspeedy
-        if(colorchange){
-            r = random(255);g = random(255);b = random(255)
-        }
-        fill(r,g,b)
-    }
-}
-function keyPressed(){
-    if(keycode ===UP_ARROW){ballspeedx *= 1.2; ballspeedy *=1.2;}
-    else if(kayCode ===DOWN_ARROW){ballspeedx *= 0.8; ballspeedy *=0.8;}
-    if(key === 'c' || key === 'C'){colorchange = !colorchange;}
-    if(key === 't' || key === 'T'){trail = !trail}
-    if(key === '+'){ballsize +=5}
-    if(key === '-'){ballsize -=5}
-}
+// let ballx = 300;
+// let bally = 200;
+// let size = 30;
+// let ballspeedx = 3;
+// let ballspeedy = 3;
+// function setup(){
+//     createCanvas(600,400);
+// }
+// function draw(){
+//     background(220);
+//     circle(ballx,bally,size);
+//     ballx += ballspeedx;
+//     bally += ballspeedy;
+//     if(ballx + (ballsize/2) >= width || ballx - (ballsize/2)<=0){
+//         ballspeedx = -ballspeedx
+//         if(colorchange){
+//             r = random(255);g = random(255);b = random(255)
+//         }
+//     }
+//     if(bally + (ballsize/2) > height || bally < ballsize/2){
+//         ballspeedy = -ballspeedy
+//         if(colorchange){
+//             r = random(255);g = random(255);b = random(255)
+//         }
+//         fill(r,g,b)
+//     }
+// }
+// function keyPressed(){
+//     if(keycode ===UP_ARROW){ballspeedx *= 1.2; ballspeedy *=1.2;}
+//     else if(kayCode ===DOWN_ARROW){ballspeedx *= 0.8; ballspeedy *=0.8;}
+//     if(key === 'c' || key === 'C'){colorchange = !colorchange;}
+//     if(key === 't' || key === 'T'){trail = !trail}
+//     if(key === '+'){ballsize +=5}
+//     if(key === '-'){ballsize -=5}
+// }
