6cb46c5e1ddfe409d231f5d06b9fbeafb468ae42
Computhink-CS140 <cs140@computhink.com.sg>
2026-08-24 10:17:59 +0000

    Aug 24, 2026, 10:17 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -11,7 +11,7 @@ function draw(){
     circle(ballx,bally,size);
     ballx += ballspeedx;
     bally += ballspeedy;
-    if(ballx + (ballsize/2) >= width || ballx - (ballsize/2)){
+    if(ballx + (ballsize/2) >= width || ballx - (ballsize/2)<=0){
         ballspeedy = -ballspeedy
     }
     if(ballx = -300){
