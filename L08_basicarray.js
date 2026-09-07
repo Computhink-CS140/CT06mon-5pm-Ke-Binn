03e806da674cb99b74c5a743299dd7bb21e9af3e
Computhink-CS140 <cs140@computhink.com.sg>
2026-09-07 10:34:15 +0000

    Sep 7, 2026, 10:34 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -59,7 +59,8 @@ let storyText = [
     "",
     "It is a period of learning...",
     "Student have begun their journey",
-    "into the world of p5.j"
+    "into the world of p5.js.",
+    
 ]
 
 
