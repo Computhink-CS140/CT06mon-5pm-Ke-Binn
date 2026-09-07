d25ee25cff01ca2f82a06ffbcbfc77ec0ddb8f66
Computhink-CS140 <cs140@computhink.com.sg>
2026-09-07 10:44:55 +0000

    Sep 7, 2026, 10:44 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -78,7 +78,7 @@ function draw(){
     textAlign(CENTER)
     for(let i=0 ; i<storyText.length ; i++){
         let sentence = storyText[i];
-        text(i*25)
+        text(width/2,yPod+i*25)
         
     }
 }
