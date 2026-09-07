7b3f4d5e2fa7372701c32a3c1ea574f633cb5871
Computhink-CS140 <cs140@computhink.com.sg>
2026-09-07 09:52:19 +0000

    Sep 7, 2026, 9:52 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,24 +1,24 @@
-let x =0
-let y = 120
-let size = 40
-let speed = 2
-let r=100
-let g=50
-let b=200
-function setup(){
-    createCanvas(400,300)
+// let x =0
+// let y = 120
+// let size = 40
+// let speed = 2
+// let r=100
+// let g=50
+// let b=200
+// function setup(){
+//     createCanvas(400,300)
 
-}
-function draw(){
-    background(220)
-    fill(r,g,b)
-    square(x,y,size)
-    x=x+speed;
+// }
+// function draw(){
+//     background(220)
+//     fill(r,g,b)
+//     square(x,y,size)
+//     x=x+speed;
 
-    if(x <= 0 || x+size>= width){
-        speed = speed * -1
-        r = random(255)
-        g = random(255)
-        b = random(255)
-    }
-}
+//     if(x <= 0 || x+size>= width){
+//         speed = speed * -1
+//         r = random(255)
+//         g = random(255)
+//         b = random(255)
+//     }
+// }
