cde3fbf7fc7772adfe1e8f4397f230a380cfc30b
Computhink-CS140 <cs140@computhink.com.sg>
2026-09-07 10:03:10 +0000

    Sep 7, 2026, 10:03 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -22,12 +22,12 @@
 //         b = random(255)
 //     }
 // }
-function setup(){
-    createCanvas(400,400)
-    textSize(15)
-    background(220)
-    text("My name is Ke Bin", 50,50)
-    text("I am 12 years old", 50,100)
-    text("My favourite activity is playing video games all day", 50,150)
-}
+// function setup(){
+//     createCanvas(400,400)
+//     textSize(15)
+//     background(220)
+//     text("My name is Ke Bin", 50,50)
+//     text("I am 12 years old", 50,100)
+//     text("My favourite activity is playing video games all day", 50,150)
+// }
 
