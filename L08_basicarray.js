7e02db15bdf33783dc9afde218a05afa93901f09
Computhink-CS140 <cs140@computhink.com.sg>
2026-09-07 10:07:46 +0000

    Sep 7, 2026, 10:07 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -37,7 +37,7 @@ function setup(){
     textSize(16)
     text("my favourite foods:", 50,50)
     for(let i = 0; i < foods.length; i++){
-        text((i+1)+ ".",50,80 )
+        text((i+1)+ ".",50,80 +i*25)
     }
 }
 
