55de9028f48cf26db3adaf0ab6625153e6c199ec
Computhink-CS140 <cs140@computhink.com.sg>
2026-09-07 09:58:49 +0000

    Sep 7, 2026, 9:58 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -28,6 +28,6 @@ function setup(){
     background(220)
     text("My name is Ke Bin", 50,50)
     text("I am 12 years old", 50,100)
-    text("I am 12 years old", 50,100)
+    text("My favourite activity is playing video games all day", 50,100)
 }
 
