adff2f55a613769cf89a49eb3542b4b5d181a11d
Computhink-CS140 <cs140@computhink.com.sg>
2026-09-14 10:10:27 +0000

    Sep 14, 2026, 10:10 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,15 +1,15 @@
 let x = 200;
-let speed = 2
-let r = 0
-let g = 0
-let b = 0
+let speed = 2;
+let r = 0;
+let g = 0;
+let b = 0;
 function setup(){
-    createCanvas(400,400)
+    createCanvas(400,400);
 }
 function draw(){
-    background(220)
-    textSize(16)
-    fill(r,g,b)
+    background(220);
+    textSize(16);
+    fill(r,g,b);
     textsize(32);
     text("Bounce!",x,200);
     x += speed;
