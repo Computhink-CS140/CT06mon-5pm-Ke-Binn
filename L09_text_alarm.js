d28c0fef287f19dd075be61b203546f721278b4a
Computhink-CS140 <cs140@computhink.com.sg>
2026-09-14 10:51:06 +0000

    Sep 14, 2026, 10:51 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -4,9 +4,9 @@ let r = 0;
 let g = 0;
 let b = 0;
 let countdown = 5;
-let bg
+let bgSound;
 function preload(){
-    bgSound
+    bgSound = loadSound()
 }
 function setup(){
     createCanvas(400,400);
