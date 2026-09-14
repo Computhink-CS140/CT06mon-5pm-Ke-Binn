055e6c063916ea729901db317df3056b2d1511c3
Computhink-CS140 <cs140@computhink.com.sg>
2026-09-14 10:52:10 +0000

    Sep 14, 2026, 10:52 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -31,6 +31,7 @@ function draw(){
     textSize(32)
     textAlign(CENTER,CENTER)
     text(textString,width/2,height/2)
+    
 }
 function mousePressed(){
     interval = setInterval(updateCountdown,1000)
