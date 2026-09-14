6cf10384c9b13eec345a1fb4beecb1f0294b9798
Computhink-CS140 <cs140@computhink.com.sg>
2026-09-14 10:10:16 +0000

    Sep 14, 2026, 10:10 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -10,13 +10,13 @@ function draw(){
     background(220)
     textSize(16)
     fill(r,g,b)
-    textsize(32)
-    text("Bounce!",x,200)
-    x += speed
+    textsize(32);
+    text("Bounce!",x,200);
+    x += speed;
     if(x > width-120 || x<0){
         speed = -speed;
-        r = random(255)
-        g = random(255)
-        b = random(255)
+        r = random(255);
+        g = random(255);
+        b = random(255);
     }
 }
