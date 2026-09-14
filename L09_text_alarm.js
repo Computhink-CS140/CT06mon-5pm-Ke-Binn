92629ad460b949133f2ef9a099bf8c8320a80e2b
Computhink-CS140 <cs140@computhink.com.sg>
2026-09-14 10:16:27 +0000

    Sep 14, 2026, 10:16 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -25,5 +25,5 @@ function draw(){
     let textString = nf(h,2) = ":" + nf(m,2) + ":" + nf(s,2);
     textsize(32)
     textAlign(CENTER,CENTER)
-    text(textString)
+    text(textString,width/2)
 }
