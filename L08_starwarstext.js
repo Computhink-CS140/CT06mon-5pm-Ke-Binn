cd4fccc0acdcf5bfdaf13e169964a66c194bdd38
Computhink-CS140 <cs140@computhink.com.sg>
2026-09-14 09:41:31 +0000

    Sep 14, 2026, 9:41 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -3,7 +3,9 @@ let r = 100
 let g = 100
 let b = 100
 let speed = 3
-let favfood = ["Potato chip","ramen","Pizza","Chicken Rice","skittles"]
+let favfood = ["Potato chip","ramen","Pizza","Chicken Rice","skittles","Fried Rice","Pasta"]
 let yPod = 500
 let lineGap = 30
+let bgSound
+let started = 
 
